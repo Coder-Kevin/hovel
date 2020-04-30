b98c458588fe0b0fec768590617d801466dac641
wangguodong <wangguodong@veredholdings.com>
2020-04-30 17:55:04 +0800

    redis example

--- src/main/java/com/hovel/redis/RedisDemo.java
+++ src/main/java/com/hovel/redis/RedisDemo.java
@@ -5,9 +5,9 @@ import org.junit.Before;
 import org.junit.Test;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.ScanResult;
+import redis.clients.jedis.Transaction;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
@@ -30,33 +30,33 @@ public class RedisDemo {
     public static Jedis jedis = null;
 
     @Before
-    public void init(){
+    public void init() {
         jedis = new Jedis();
     }
 
     @After
-    public void close(){
-        if(jedis != null){
+    public void close() {
+        if (jedis != null) {
             jedis.close();
         }
     }
 
     @Test
-    public void list(){
+    public void list() {
         jedis.lpush("mylist", "1");
         jedis.lpush("mylist", "2");
         jedis.lpush("mylist", "3");
         jedis.lpush("mylist", "4");
 
-        List<String> list = jedis.lrange("mylist",0,2);
+        List<String> list = jedis.lrange("mylist", 0, 2);
 
         System.out.println(Arrays.toString(list.toArray()));
     }
 
     @Test
-    public void keys(){
+    public void keys() {
         Set<String> keys = jedis.keys("my*");
-        if(keys != null && !keys.isEmpty()) {
+        if (keys != null && !keys.isEmpty()) {
             keys.forEach(key -> System.out.println(key));
         }
     }
@@ -64,8 +64,74 @@ public class RedisDemo {
     @Test
     public void scan() {
         ScanResult<String> scanResult = jedis.scan("0");
-        
+        List<String> list = scanResult.getResult();
+        System.out.println(Arrays.toString(list.toArray()));
+    }
 
+
+    @Test
+    public void multi() {
+        Transaction transaction = jedis.multi();
+        transaction.set("mu", "ha");
+        transaction.zadd("mu", 1, "ha");
+        List<Object> execResult = transaction.exec();
+        System.out.println(Arrays.toString(execResult.toArray()));
+
+        System.out.println(jedis.get("mu"));
+    }
+
+
+    @Test
+    public void multiNotSafe() {
+        Jedis anotherJedis = new Jedis();
+
+        jedis.set("mu","hello");
+        Transaction transaction = jedis.multi();
+        transaction.append("mu", "1");
+        anotherJedis.append("mu", "hu");
+        List<Object> execResult = transaction.exec();
+        System.out.println(Arrays.toString(execResult.toArray()));
+        System.out.println(anotherJedis.get("mu"));
+        anotherJedis.close();
+    }
+
+    @Test
+    public void multiSafe() {
+        Jedis anotherJedis = new Jedis();
+
+        Transaction transaction = jedis.multi();
+        transaction.set("mu", "hi"); // 存在这一句，结果不会出问题
+        transaction.append("mu", "1");
+        anotherJedis.append("mu", "hu");
+        List<Object> execResult = transaction.exec();
+        System.out.println(Arrays.toString(execResult.toArray()));
+        System.out.println(anotherJedis.get("mu"));
+        System.out.println(jedis.get("mu"));
+        anotherJedis.close();
+    }
+
+    @Test
+    public void multiWatch() {
+        Jedis anotherJedis = new Jedis();
+        jedis.set("mu", "1");
+
+        // 监视一个(或多个) key ，如果在事务执行之前这个(或这些) key 被其他命令所改动，那么事务将被打断。
+        jedis.watch("mu");
+        Transaction transaction = jedis.multi();
+        transaction.append("mu", "1");
+        transaction.set("new1", "2");
+        anotherJedis.append("mu", "hu");
+        List<Object> execResult = transaction.exec();
+
+        if (execResult == null || execResult.isEmpty()) {
+            System.out.println(jedis.get("mu"));
+            // 乐观锁
+        } else {
+            System.out.println(Arrays.toString(execResult.toArray()));
+            System.out.println(jedis.get("mu"));
+        }
+
+        anotherJedis.close();
     }
 
 }
