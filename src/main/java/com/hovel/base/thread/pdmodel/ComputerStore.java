fc1adf38641b46b020fd0302e9a3dc9dfffc397b
wangguodong <wangguodong@veredholdings.com>
2020-05-06 11:16:52 +0800

    生产者消费者example

--- src/main/java/com/hovel/base/thread/pdmodel/ComputerStore.java
+++ src/main/java/com/hovel/base/thread/pdmodel/ComputerStore.java
@@ -1,34 +1,60 @@
 package com.hovel.base.thread.pdmodel;
 
+import lombok.SneakyThrows;
+
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class ComputerStore {
 
-    private static volatile int count = 0;
+    private static AtomicInteger count = new AtomicInteger();
 
-    private static List<Computer> computers = new ArrayList<>();
+    private static final List<Computer> computers = new ArrayList<>();
 
-    public synchronized void create(String type, String num, String name) throws InterruptedException {
-        if (count == 1000) {
-            System.out.println(Thread.currentThread().getName() + "发现仓库已经满了");
+    private final Object lock = new Object();
+
+    public void create(String type, String num, String name) throws InterruptedException {
+
+        synchronized (lock) {
+            System.out.println("生产者" + Thread.currentThread().getName() + " get lock");
+            while (count.intValue() == 100) {
+                System.out.println(Thread.currentThread().getName() + "发现仓库已经满了");
+                lock.wait();
+            }
+            Computer computer = new Computer(type, num, name);
+            computers.add(computer);
+            count.addAndGet(1);
+            lock.notifyAll();
+            System.out.println(Thread.currentThread().getName() + " - 生产电脑：" + computer + ",理论库存：" + count + ",实际库存" + computers.size());
+            System.out.println("生产者" + Thread.currentThread().getName() + " release lock");
         }
-        Computer computer = new Computer(type, num, name);
-        computers.add(computer);
-        count++;
-        System.out.println(Thread.currentThread().getName() + " - 生产电脑：" + computer + ",理论库存：" + count + ",实际库存" + computers.size());
     }
+    @SneakyThrows
+    public void consume() {
+        synchronized(lock) {
+            System.out.println("消费者" + Thread.currentThread().getName() + " get lock");
+            while (!hasProduct()) {
+                lock.wait();
+            }
+            if (count.intValue() != 0) {
+                Computer computer = computers.remove(computers.size() - 1);
+                count.decrementAndGet();
+                System.out.println(Thread.currentThread().getName() + " - 买走电脑：" + computer + ",理论库存：" + count.intValue() + ",实际库存" + computers.size());
+            }
 
-    public synchronized void consume() {
-        if (count != 0) {
-            Computer computer = computers.remove(computers.size() - 1);
-            count--;
-            System.out.println(Thread.currentThread().getName() + " - 买走电脑：" + computer + ",理论库存：" + count + ",实际库存" + computers.size());
-
+            if (!full()) {
+                lock.notifyAll();
+            }
+            System.out.println("消费者" + Thread.currentThread().getName() + " release lock");
         }
     }
 
     public boolean full(){
-        return count == 1000;
+        return count.intValue() == 100;
+    }
+
+    public boolean hasProduct(){
+        return count.intValue() > 0;
     }
 
 }
