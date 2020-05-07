21c13b19a8b17997b1a011bb1a3ef98238b4ac49
wangguodong <wangguodong@veredholdings.com>
2020-05-07 16:05:29 +0800

    CyclicBarrierTest

---
+++ src/main/java/com/hovel/base/thread/future/FutureDemo.java
@@ -0,0 +1,54 @@
+package com.hovel.base.thread.future;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.*;
+
+public class FutureDemo {
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(1,
+                1,
+                1l,
+                TimeUnit.SECONDS,
+                new ArrayBlockingQueue<Runnable>(1),
+                new ThreadPoolExecutor.DiscardPolicy());
+
+        Future future1 = executor.submit(new Runnable() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                System.out.println("start task 1");
+                Thread.sleep(1000);
+            }
+        });
+
+        Future future2 = executor.submit(new Runnable() {
+            @SneakyThrows
+            @Override
+            public void run() {
+                System.out.println("start task 2");
+            }
+        });
+
+
+        Future future3 = null;
+        try {
+            future3 = executor.submit(new Runnable() {
+                @SneakyThrows
+                @Override
+                public void run() {
+                    System.out.println("start task 3");
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("return task 1 :" + future1.get());
+        System.out.println("return task 2 :" + future2.get());
+//        System.out.println("return task 3 :" + future3 == null ? null : future3.get());
+
+        executor.shutdown();
+    }
+}
---
+++ src/main/java/com/hovel/base/thread/lock/CyclicBarrierTest.java
@@ -0,0 +1,40 @@
+package com.hovel.base.thread.lock;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+
+public class CyclicBarrierTest {
+
+    private static CyclicBarrier cBarrier = null;
+
+    public static void main(String[] args) {
+        cBarrier = new CyclicBarrier(5,new Runnable() {
+
+            @Override
+            public void run() {
+                System.out.println("人到齐了，开会吧");
+            }
+        });
+
+        for(int i=0;i<5;i++){
+            Thread party = new PartiesThread();
+            party.start();
+//            if(i == 4){
+//                party.interrupt();
+//            }
+        }
+    }
+
+    static class PartiesThread extends Thread{
+        public void run(){
+            System.out.println(Thread.currentThread().getName()+"到了会议室");
+            try {
+                cBarrier.await();
+
+            } catch (InterruptedException | BrokenBarrierException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
