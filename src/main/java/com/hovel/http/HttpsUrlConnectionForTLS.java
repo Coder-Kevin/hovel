68b1c1f3106ff09baa5be1cce134633096571118
Kevin <841156602@qq.com>
2020-09-05 21:39:06 +0800

    jdk7请求https支持TLSv1.2

---
+++ src/main/java/com/hovel/http/HttpsUrlConnectionForTLS.java
@@ -0,0 +1,123 @@
+package com.hovel.http;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.net.*;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Kevin
+ */
+public class HttpsUrlConnectionForTLS {
+
+    public static final String APPID = "";
+    public static final String URL = "";
+    public static final String ACCESS_KEY = "";
+    public static final String REPLACE_STR = "\r\n";
+
+    public HttpURLConnection createConnection(URI uri) throws IOException {
+        URL url = uri.toURL();
+        URLConnection connection = url.openConnection();
+        HttpsURLConnection httpsURLConnection = (HttpsURLConnection) connection;
+        httpsURLConnection.setSSLSocketFactory(new TLSSocketConnectionFactory());
+        return httpsURLConnection;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        String apiUrl = URL;
+        /*接口参数包装*/
+        String json = "json body";
+        Map paramMap = JSONObject.parseObject(json, Map.class);
+
+        String newparams = JSON.toJSONString(paramMap);
+        System.out.println(apiUrl);
+
+        /*每次接口调用生成随机加密字符串*/
+        String x_bigtree_nonce = Utils.makeRandomStr(16);
+
+        //AES加密(去掉空格和换行)
+        String aesJsonStr = Utils.encrypt(newparams, ACCESS_KEY, x_bigtree_nonce).replaceAll(REPLACE_STR, "");
+        //签名规则
+        String signRule = ACCESS_KEY + aesJsonStr + APPID + x_bigtree_nonce;
+        //MD5签名(去掉空格和换行)
+        String sign = Utils.md5(signRule.replaceAll(REPLACE_STR, ""));
+        Map<String, String> param = new HashMap<String, String>();
+        param.put("data", URLEncoder.encode(aesJsonStr, "UTF-8"));
+        System.out.println("nonce:" + x_bigtree_nonce + ",sign:" + sign);
+        System.out.println("sign:" + sign);
+        System.out.println("data:" + param.get("data"));
+        System.out.println("requestParam:" + JSON.toJSONString(param));
+
+
+        // post请求是将参数放在请求体里面传过去的;这里将entity放入post请求体中
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Content-Type", "application/json;charset=utf8");
+        headers.put("x-bigtree-appid", APPID);
+        headers.put("x-bigtree-nonce", x_bigtree_nonce);
+        headers.put("x-bigtree-sign", sign);
+        headers.put("Content-Type", "application/json;charset=utf-8");
+
+        doPost(apiUrl, JSON.toJSONString(param), headers);
+    }
+
+    private static void doPost(String url, String body, Map<String, String> headers) {
+        HttpsUrlConnectionForTLS httpsUrlConnectionMessageSender = new HttpsUrlConnectionForTLS();
+        HttpURLConnection connection;
+        try {
+            connection = httpsUrlConnectionMessageSender.createConnection(new URI(url));
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            connection.setRequestMethod("POST");
+            connection.setUseCaches(false);
+            connection.setInstanceFollowRedirects(true);
+            if (headers != null && headers.size() > 0) {
+                for (String key : headers.keySet()) {
+                    connection.setRequestProperty(key, headers.get(key));
+                }
+            }
+
+            connection.connect();
+            //POST请求
+            OutputStreamWriter os = null;
+            String json = "";
+            os = new OutputStreamWriter(connection.getOutputStream());
+            os.write(body);
+            os.flush();
+            json = getResponse(connection);
+            System.out.println(json);
+
+            if (connection != null) {
+                connection.disconnect();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getResponse(HttpURLConnection Conn) throws IOException {
+        InputStream is;
+        if (Conn.getResponseCode() >= 400) {
+            is = Conn.getErrorStream();
+        } else {
+            is = Conn.getInputStream();
+        }
+        String response = "";
+        byte buff[] = new byte[512];
+        int b = 0;
+        while ((b = is.read(buff, 0, buff.length)) != -1) {
+            response += new String(buff, 0, b);
+
+        }
+        is.close();
+        return response;
+    }
+}
---
+++ src/main/java/com/hovel/http/Utils.java
@@ -0,0 +1,98 @@
+package com.hovel.http;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.util.Random;
+
+
+public class Utils {
+
+    /**
+     * @Description: 加密
+     * @Param: [raw, aes_key, aes_iv]
+     * @Return: java.lang.String
+     */
+    public static String encrypt(String raw, String key, String iv) throws Exception {
+        System.out.println("raw:" + raw);
+        try {
+            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+            int blockSize = cipher.getBlockSize();
+            byte[] dataBytes = raw.getBytes();
+            int dataLenth = dataBytes.length;
+            if (dataLenth % blockSize != 0) {
+                dataLenth = dataLenth + (blockSize - (dataLenth % blockSize));
+            }
+            byte[] plaintext = new byte[dataLenth];
+            System.arraycopy(dataBytes, 0, plaintext, 0, dataBytes.length);
+            SecretKeySpec keyspec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes());
+            cipher.init(Cipher.ENCRYPT_MODE, keyspec, ivspec);
+            System.out.println("plaintext:" + plaintext);
+            byte[] encrypted = cipher.doFinal(plaintext);
+            return new sun.misc.BASE64Encoder().encode(encrypted).replaceAll("\r\n", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * @Description: 解密
+     * @Date: 2019/8/28 13:55
+     * @Param: [data, key, iv]
+     * @Return: java.lang.String
+     */
+    public static String desEncrypt(String data, String key, String iv) throws Exception {
+        try {
+
+            byte[] encrypted1 = new sun.misc.BASE64Decoder().decodeBuffer(data);
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/NoPadding");
+            SecretKeySpec keyspec = new SecretKeySpec(key.getBytes(), "AES");
+            IvParameterSpec ivspec = new IvParameterSpec(iv.getBytes());
+
+            cipher.init(Cipher.DECRYPT_MODE, keyspec, ivspec);
+
+            byte[] original = cipher.doFinal(encrypted1);
+            String originalString = new String(original, "UTF-8");
+            return originalString.trim();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * 16位随机密码生成
+     */
+    public static String makeRandomStr(int len) {
+        char charr[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        Random r = new Random();
+
+        for (int x = 0; x < len; ++x) {
+            sb.append(charr[r.nextInt(charr.length)]);
+        }
+        return sb.toString();
+    }
+
+
+    public static String md5(String str) {
+        try {
+            // 生成一个MD5加密计算摘要
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            // 计算md5函数
+            md.update(str.getBytes());
+            // digest()最后确定返回md5 hash值，返回值为8位字符串。因为md5 hash值是16位的hex值，实际上就是8位的字符
+            // BigInteger函数则将8位的字符串转换成16位hex值，用字符串来表示；得到字符串形式的hash值
+            //一个byte是八位二进制，也就是2位十六进制字符（2的8次方等于16的2次方）
+            return new BigInteger(1, md.digest()).toString(16);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
