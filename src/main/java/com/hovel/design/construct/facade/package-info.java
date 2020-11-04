84f3bb08e8c33169516610b4b2e687d4aa3fa2f7
KevinKing <841156602@qq.com>
2020-11-04 22:58:25 +0800

    享元模式和门面模式记录

---
+++ src/main/java/com/hovel/design/construct/facade/package-info.java
@@ -0,0 +1,10 @@
+package com.hovel.design.construct.facade;
+
+/**
+ * 门面模式  外观模式
+ *
+ * 提供统一的入口，方便系统及各子系统间的访问
+ *
+ * 网关服务就是一种典型门面模式
+ *
+  */
---
+++ src/main/java/com/hovel/design/construct/flyweight/package-info.java
@@ -0,0 +1,8 @@
+package com.hovel.design.construct.flyweight;
+
+/**
+ * 享元模式
+ *
+ * 使用场景：大量相似对象；需要缓冲池的地方；
+ * 比如：jdk中String字符串缓冲池；数据库连接池
+ */
